95bc858aad94386262274c949412d4fe4dbfdbcf
fox1985 <artemdav2gmail.com>
2020-04-21 21:10:47 +0600

    commit

--- listings/views.py
+++ listings/views.py
@@ -9,6 +9,8 @@ from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from .choices import price_choices, bedroom_choices, state_choices
 from order.forms import OrderForm
 from django.core.urlresolvers import reverse
+from django.views.generic.list import ListView
+from django.db.models import Q
 
 # Create your views here.
 
@@ -87,47 +89,16 @@ def listing(request, listing_id):
 
 
 
+class SearchListView(ListView):
+    model = Listing
+    template_name = 'pages/index.html'
+    context_object_name = 'listings'
 
-def search(request):
-  "Посик"
-  queryset_list = Listing.objects.order_by('-list_date')
-
-  # Keywords
-  if 'keywords' in request.GET:
-    keywords = request.GET['keywords']
-    if keywords:
-      queryset_list = queryset_list.filter(description__icontains=keywords)
-
-  # City
-  if 'city' in request.GET:
-    city = request.GET['city']
-    if city:
-      queryset_list = queryset_list.filter(city__iexact=city)
-
-  # State
-  if 'state' in request.GET:
-    state = request.GET['state']
-    if state:
-      queryset_list = queryset_list.filter(state__iexact=state)
-
-  # Bedrooms
-  if 'bedrooms' in request.GET:
-    bedrooms = request.GET['bedrooms']
-    if bedrooms:
-      queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
-
-  # Price
-  if 'price' in request.GET:
-    price = request.GET['price']
-    if price:
-      queryset_list = queryset_list.filter(price__lte=price)
-
-  context = {
-    'state_choices': state_choices,
-    'bedroom_choices': bedroom_choices,
-    'price_choices': price_choices,
-    'listings': queryset_list,
-    'values': request.GET
-  }
-
-  return render(request, 'listings/search.html', context)
+    def get_queryset(self):
+      # Получаем не отфильтрованный кверисет всех моделей
+      queryset = Listing.objects.all()
+      q = self.request.GET.get("q")
+      if q:
+        # Если 'q' в GET запросе, фильтруем кверисет по данным из 'q'
+        return queryset.filter(Q(bathrooms=q), Q(rooms=q))
+      return queryset
