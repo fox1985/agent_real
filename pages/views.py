c25db69c02feee7ca63ace37fe1e70a995d3d8c4
fox1985 <artemdav2gmail.com>
2020-04-28 17:17:07 +0600

    commit

--- pages/views.py
+++ pages/views.py
@@ -1,1 +1,22 @@
 # -*- coding: utf-8 -*-
+from django.db.models import Q
+from django.views.generic import TemplateView, ListView
+
+from listings.models import Listing
+
+
+class HomePageView(TemplateView):
+    template_name = 'listings/listings.html'
+
+
+class SearchResultsView(ListView):
+    model = Listing
+    template_name = 'pages/search_results.html'
+    context_object_name = 'listings'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = Listing.objects.filter(
+            Q(bedrooms__contains=query) | Q(rooms__icontains=query)
+        )
+        return object_list
