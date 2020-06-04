c93446c43236c33d627f7a5d44fcaf0bb155d634
fox1985 <artemdav2gmail.com>
2020-06-04 17:28:56 +0600

    commit

--- listings/admin.py
+++ listings/admin.py
@@ -18,11 +18,11 @@ class Galary_image_Admin(admin.TabularInline):
 
 class ListingAdmin(admin.ModelAdmin):
   inlines = [Galary_image_Admin]
-  list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+  list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor', 'floors')
   list_display_links = ('id', 'title')
   list_filter = ('realtor',)
   list_editable = ('is_published',)
-  search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price', 'floors')
+  search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price', )
   list_per_page = 25
 
 
